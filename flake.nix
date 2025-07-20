75a721184e8684d5d780bdf7d3ae19af5559c63b
vyls <git@feils.me>
2025-07-20 13:27:46 +0200

    feat: modernize flake with current best practices
    - Add flake-utils for better system handling
    - Use proper package structure with default and named packages
    - Export nixosModules and homeManagerModules (both default and named)
    - Add devShells with proper development dependencies
    - Add formatter and checks outputs
    - Update to cleaner input structure
    - Support direct module imports in system configurations

--- flake.nix
+++ flake.nix
@@ -1,27 +1,58 @@
 {
-  description = "My Nix based Configurations";
+  description = "A wrapper for pkg-config that uses nixpkgs packages";
 
   inputs = {
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.follows = "nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, ... }@inputs:
+    { self, nixpkgs, flake-utils, ... }:
     let
       inherit (self) outputs;
-      systems = [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      forAllSystems = inputs.nixpkgs.lib.genAttrs systems;
+      systems = flake-utils.lib.defaultSystems;
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default ];
+      };
     in
     {
-      packages = forAllSystems (system: import ./pkgs inputs.nixpkgs.legacyPackages.${system});
-      formatter = forAllSystems (system: inputs.nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
-      overlays = import ./overlays { inherit inputs outputs; };
+      # Packages for each system
+      packages = forEachSystem (system: {
+        default = self.packages.${system}.nix-pkgconfig;
+        nix-pkgconfig = (pkgsFor system).nix-pkgconfig;
+        nix-pkgconfig-wrapped = (pkgsFor system).nix-pkgconfig-wrapped;
+      });
+
+      # Overlays
+      overlays = import ./overlays { inputs = {}; inherit outputs; };
+
+      # NixOS and Home Manager modules
+      nixosModules.default = import ./modules/nixos.nix;
+      homeManagerModules.default = import ./modules/home-manager.nix;
+      
+      # Config modules for direct import
+      nixosModules.nix-pkgconfig = import ./modules/nixos.nix;
+      homeManagerModules.nix-pkgconfig = import ./modules/home-manager.nix;
+
+      # Development shell
+      devShells = forEachSystem (system: {
+        default = (pkgsFor system).mkShell {
+          buildInputs = with (pkgsFor system); [
+            python3
+            nix-index
+            nixfmt-rfc-style
+          ];
+        };
+      });
+
+      # Formatter
+      formatter = forEachSystem (system: (pkgsFor system).nixfmt-rfc-style);
+
+      # Checks
+      checks = forEachSystem (system: {
+        nix-pkgconfig = self.packages.${system}.nix-pkgconfig;
+      });
     };
 }
